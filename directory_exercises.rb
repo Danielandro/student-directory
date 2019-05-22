4ad10e7da7443ffb0ea5d961803233b505d2d8a5
Danielandro <mtme13@hotmail.com>
2019-05-22 21:07:35 +0000

    Add plural check for student(s)

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -15,7 +15,7 @@ def input_students
     student_hash[:cohort] = cohort if !cohort.empty?
     # add the student hash to the array
     students << student_hash
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} #{check_if_plural(students)}"
     # get another name from the user
     puts "Enter next student"
     name = gets.chomp
@@ -24,6 +24,14 @@ def input_students
   end
   # return array of students
   students
+end
+
+def check_if_plural(students)
+  if students.count > 1
+    "students"
+  else
+    "student"
+  end
 end
 
 def filter_by_cohort(students)
@@ -42,7 +50,7 @@ end
 def print(students, cohorts=nil)
   cohorts.each do |cohort|
     student_num = 0
-    puts "All the students from the #{cohort.capitalize} cohort:"
+    puts "Student(s) from the #{cohort.capitalize} cohort:"
     while student_num < students.size 
       if students[student_num][:cohort] == cohort
         matching_student = "#{students[student_num][:name]}"
@@ -54,7 +62,7 @@ def print(students, cohorts=nil)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great #{check_if_plural(students)}"
 end
 
 students = input_students
