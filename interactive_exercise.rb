41760844f43879c81352ac5695cca823abd22e9b
Danielandro <mtme13@hotmail.com>
2019-05-23 16:46:56 +0000

    Refactor interactive menu

--- interactive_exercise.rb
+++ interactive_exercise.rb
@@ -1,20 +1,52 @@
+@students = [] #an empty array accesible to all methods
+
+def interactive_menu
+  loop do
+    print_menu
+    #read the input and perform action
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  #print menu & ask user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students #input the students
+    when "2"
+      show_students #show the students
+    when "9"
+      exit #exit program
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
+  #get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
+  #while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    #add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    #get another name from the user
     name = gets.chomp
   end
-   # return the array of students
-  students
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -22,43 +54,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-
-def interactive_menu
-  students = []
-  loop do
-    #1. print menu & ask user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    #2. read the input and save it to variable
-    selection = gets.chomp
-    
-    case selection
-      when "1"
-        #input the students
-        students = input_students
-      when "2"
-        #show the students
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit #exit program
-      else
-        puts "I don't know what you meant, try again"
-    end
-  end
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
